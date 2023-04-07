72c69a812c21f6350162b3cbebdfa01d213b1f42
Jonnier Andres Teran Morales <jonnierteranmorales@gmail.com>
2023-04-06 19:04:40 -0500

    Creacion y Migracion de Modelos a entidades a bd

---
+++ src/main/java/com/example/Backend/Models/TareaModel.java
@@ -0,0 +1,82 @@
+package com.example.Backend.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+/**
+ * TareaModel
+ */
+
+@Entity
+@Table(name = "Tarea")
+public class TareaModel {
+
+    @Id // Indicamos que es un id  primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Se genera automaticamente y Es Autoincrementable
+    @Column(unique = true, nullable = false) // Es unico  y no nullo
+    private Long id;
+
+    @Column(nullable = false  , columnDefinition = "VARCHAR(250) ") //  no nullo , y tamaño maximo de 255 bits
+    private String titulo;
+
+    @Column(nullable = false  , columnDefinition = "VARCHAR(250) ") //  no nullo , y tamaño maximo de 255 bits
+    private String descripcion;
+
+    @Column(nullable = false  , columnDefinition = "VARCHAR(250) DEFAULT 'Pendiente' ") //  no nullo , y tamaño maximo de 255 bits
+    private String estado;
+
+    @ManyToOne // Relacion de muchos a uno
+    @JoinColumn(nullable = false) //  no nullo 
+    private UserModel id_user;
+     
+    public TareaModel(){}
+    
+    public void setId(Long id){
+        this.id= id;
+    }
+    public Long Getid(){
+        return this.id;
+    }
+
+    public void SetTitulo(String Titulo){
+        this.titulo = Titulo;
+    }
+
+    public String getTitulo(){
+        return this.titulo;
+    }
+
+    public void setDescripcion(String Descripcion){
+        this.descripcion = Descripcion;
+    }
+
+    public String getDrescString(){
+        return this.descripcion;
+    }
+
+    public void setEstdado(String Estado){
+        this.estado = Estado;
+    }
+
+    public String getEstado(){
+        return this.estado;
+    }
+
+    public void setId_user(UserModel id_user){
+        this.id_user = id_user;
+    }
+
+    public UserModel getId_user(){
+        return this.id_user;
+    }
+
+   
+
+    
+}
---
+++ src/main/java/com/example/Backend/Models/UserModel.java
@@ -0,0 +1,101 @@
+package com.example.Backend.Models;
+
+
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+/**
+ * UserModel
+ */
+@Entity // Indicador de que es un modelo Real para una Base de datos
+@Table(name = "Usuario") // Indicamos el nombre que queremos para la tabla en la bd para que no use el de la clase
+public class UserModel {
+    //Propiedades de la entidas / clase
+
+    @Id // Indicamos que es un id  primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Se genera automaticamente y Es Autoincrementable
+    @Column(unique = true, nullable = false) // Es unico  y no nullo
+    private Long id;
+
+    @Column(nullable = false  , columnDefinition = "VARCHAR(250) ") //  no nullo , y tamaño maximo de 255 bits
+    private String nombres;
+
+    @Column(nullable = false , columnDefinition = "VARCHAR(250) ") //  no nullo , y tamaño maximo de 255 bits
+    private String apellidos;
+
+    @Column(nullable = false,  columnDefinition = "VARCHAR(250) ") //  no nullo , y tamaño maximo de 255 bits
+    private String email;
+
+    @Column(nullable = false ,  columnDefinition = "VARCHAR(250) ") //  no nullo , y tamaño maximo de 255 bits 
+    private String contraseña;
+
+
+    // Relentiza el proyecto al cargar muuchos datos
+   @OneToMany(mappedBy = "id_user") //Relacion bidireccional de uno a muchos para el mapeo de datos no se refleja en la tabla sql
+    private List<TareaModel> tareas;  //Lista de tareas de un usuario especifico
+    // private set<TareaModel> Tareas;  //Lista de tareas de un usuario especifico
+
+    
+    public UserModel(){}
+
+    //Sett y gett
+    public void setId(Long Id){
+        this.id = Id;
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public void setNombres(String nombres){
+        this.nombres = nombres;
+    }
+
+    public String getNombres(){
+        return this.nombres;
+    }
+    
+    public void setApellidos(String Apellidos){
+        this.apellidos = Apellidos;
+    }
+
+    public String getApellidos(){
+        return this.apellidos;
+    }
+
+    public void setEmail(String Email){
+        this.email = Email;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setContraseña(String Contraseña){
+        this.contraseña = Contraseña;
+    }
+
+    public String getContraseña(){
+        return this.contraseña;
+    }
+
+    public void setTareas(List<TareaModel> Tareas){
+        this.tareas = Tareas;
+    }
+
+    public List<TareaModel> getTareas(){
+        return this.tareas;
+    }
+  
+}
+
+
+   
